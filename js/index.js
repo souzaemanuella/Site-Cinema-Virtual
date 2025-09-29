e94cba47a63457ca245d499541b2014b6154ab85
Marianamachado455 <mariana.25104708360006@faeterj-rio.edu.br>
2025-09-29 10:35:01 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,26 +1,28 @@
 const filmesCompletos = {
-  1:  { id: 1,  genero: "acao",     titulo: "Avatar",                            idade: "12", poster: "Imagens/poster5.jpeg" },
-  2:  { id: 2,  genero: "acao",     titulo: "Vingadores: Ultimato",              idade: "12", poster: "Imagens/poster6.jpeg" },
-  3:  { id: 3,  genero: "fantasia", titulo: "Toy Story 4",                       idade: "L",  poster: "Imagens/foto3_filme.avif" },
-  4:  { id: 4,  genero: "drama",    titulo: "Titanic",                           idade: "12", poster: "Imagens/poster7.jpg" },
-  5:  { id: 5,  genero: "drama",    titulo: "A longa jornada",                   idade: "12", poster: "Imagens/foto1_filme.avif" },
-  6:  { id: 6,  genero: "comedia",  titulo: "A Grande Viagem de sua vida",       idade: "12", poster: "Imagens/poster2.jpeg" },
-  7:  { id: 7,  genero: "fantasia", titulo: "Ne Zha 2",                          idade: "L",  poster: "Imagens/poster3.png" },
-  8:  { id: 8,  genero: "acao",     titulo: "Superman",                          idade: "12", poster: "Imagens/poster4.jpg" },
-  
-  // Adicionando os outros filmes para a seção de gênero funcionar
-  9:  { id: 9,  genero: "acao",     titulo: "Mad Max: Estrada da Fúria",        idade: "14", poster: "Imagens/postermadmax.jpg" },
-  10: { id: 10, genero: "acao",     titulo: "John Wick",                         idade: "16", poster: "Imagens/poster_jhonwick.jpg" },
-  11: { id: 11, genero: "comedia",  titulo: "Se beber não case",                 idade: "14", poster: "Imagens/poster_Sebebernaocase.jpg" },
-  12: { id: 12, genero: "comedia",  titulo: "Deadpool e Wolverine",              idade: "16", poster: "Imagens/poster_deadpool.webp" },
-  13: { id: 13, genero: "comedia",  titulo: "Esqueceram de mim 2",               idade: "L",  poster: "Imagens/poster_esqueceramDeMIm.webp" },
-  14: { id: 14, genero: "drama",    titulo: "O poderoso chefão",                idade: "16", poster: "Imagens/poster_PoderosoChefao.webp" },
-  15: { id: 15, genero: "drama",    titulo: "A vida é bela",                     idade: "12", poster: "Imagens/poster_VidaéBela.jpeg" },
-  16: { id: 16, genero: "terror",   titulo: "It:A coisa",                        idade: "16", poster: "Imagens/poster_it.jpeg" },
-  17: { id: 17, genero: "terror",   titulo: "Invocação do mal",                  idade: "16", poster: "Imagens/poster_InvocaoDoMal.png" },
-  18: { id: 18, genero: "terror",   titulo: "Saw",                               idade: "18", poster: "Imagens/poster_saw.jpg" },
-  19: { id: 19, genero: "terror",   titulo: "O exorcista",                       idade: "18", poster: "Imagens/poster_OExorcista.jpg" },
-  20: { id: 20, genero: "fantasia", titulo: "Wicked",                            idade: "L",  poster: "Imagens/poster wicked.jpeg" }
+  1:  { id: 1,  genero: "acao",     titulo: "Vingadores",                        idade: "12", poster: "Imagens/poster6.jpeg" },
+  2:  { id: 2,  genero: "acao",     titulo: "Superman",                          idade: "12", poster: "Imagens/poster4.jpg" },
+  3:  { id: 3,  genero: "acao",     titulo: "Mad Max: Estrada da Fúria",        idade: "14", poster: "Imagens/postermadmax.jpg" },
+  4:  { id: 4,  genero: "acao",     titulo: "John Wick",                         idade: "16", poster: "Imagens/poster_jhonwick.jpg" },
+
+  5:  { id: 5,  genero: "comedia",  titulo: "A Grande Viagem de Sua Vida",       idade: "L",  poster: "Imagens/poster2.jpeg" },
+  6:  { id: 6,  genero: "comedia",  titulo: "Se beber não case",                 idade: "14", poster: "Imagens/poster_Sebebernaocase.jpg" },
+  7:  { id: 7,  genero: "comedia",  titulo: "Deadpool e Wolverine",              idade: "16", poster: "Imagens/poster_deadpool.webp" },
+  8:  { id: 8,  genero: "comedia",  titulo: "Esqueceram de mim 2",               idade: "L",  poster: "Imagens/poster_esqueceramDeMIm.webp" },
+
+  9:  { id: 9,  genero: "drama",    titulo: "A longa jornada",                   idade: "12", poster: "Imagens/foto1_filme.avif" },
+  10: { id: 10, genero: "drama",    titulo: "Titanic",                           idade: "14", poster: "Imagens/poster7.jpg" },
+  11: { id: 11, genero: "drama",    titulo: "O poderoso chefão",                idade: "16", poster: "Imagens/poster_PoderosoChefao.webp" },
+  12: { id: 12, genero: "drama",    titulo: "A vida é bela",                     idade: "12", poster: "Imagens/poster_VidaéBela.jpeg" },
+
+  13: { id: 13, genero: "terror",   titulo: "It:A coisa",                        idade: "16", poster: "Imagens/poster_it.jpeg" },
+  14: { id: 14, genero: "terror",   titulo: "Invocação do mal",                  idade: "16", poster: "Imagens/poster_InvocaoDoMal.png" },
+  15: { id: 15, genero: "terror",   titulo: "Saw",                               idade: "18", poster: "Imagens/poster_saw.jpg" },
+  16: { id: 16, genero: "terror",   titulo: "O exorcista",                       idade: "18", poster: "Imagens/poster_OExorcista.jpg" },
+
+  17: { id: 17, genero: "fantasia", titulo: "Wicked",                            idade: "L",  poster: "Imagens/poster wicked.jpeg" },
+  18: { id: 18, genero: "fantasia", titulo: "Toy story 4",                       idade: "L",  poster: "Imagens/foto3_filme.avif" },
+  19: { id: 19, genero: "fantasia", titulo: "Avatar",                            idade: "12", poster: "Imagens/poster5.jpeg" },
+  20: { id: 20, genero: "fantasia", titulo: "Ne Zha 2",                          idade: "14", poster: "Imagens/poster3.png" }
 };
 
 const cores = {
@@ -90,8 +92,9 @@ function mostrarFilmes(genero) {
 mostrarFilmes('acao');
 
 function removerAcento(texto) {
-  const genero = removerAcento(op.textContent.trim());
+  return texto.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase().trim();
 }
+
 
 document.querySelectorAll("#generos .opcoes_genero").forEach(op => {
     op.addEventListener("click", () => {
@@ -103,3 +106,4 @@ document.querySelectorAll("#generos .opcoes_genero").forEach(op => {
         mostrarFilmes(genero);
     });
 });
+
