ff414d3950cc2521097848a5d15e07a8eda14c20
souzaemanuella <emanuella.25104708360004@faeterj-rio.edu.br>
2025-09-29 20:04:45 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const button = document.getElementById('pagamento');
+const alerta = document.getElementById('alerta');
+
+button.addEventListener('click', function(event) {
+    event.preventDefault();
+    alerta.style.display = 'block';
+    setTimeout(() => {
+        alerta.style.display = 'none';
+    }, 3000);
+})
